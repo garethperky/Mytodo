485fc8ef0f5bcc397bfa4afa86093aff38c59a4e
Gareth Perkins <g.perkins@rcvs.org.uk>
2019-10-13 12:34:49 +0100

    chore: link user to todo

--- todo/todonow/models.py
+++ todo/todonow/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -6,6 +7,7 @@ class Todo(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def _str_(self):
         return self.title
