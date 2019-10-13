b6321543cff35d3a03c946a7824680039197cb28
Gareth Perkins <g.perkins@rcvs.org.uk>
2019-10-13 16:09:11 +0100

    chore: added profile pictures

--- todo/todonow/urls.py
+++ todo/todonow/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- todo/todonow/views.py
+++ todo/todonow/views.py
@@ -3,9 +3,16 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Todo
+from django.contrib.auth.models import User
 
 
 @login_required
 def index(request):
     todo = Todo.objects.filter(user=request.user)
+    print("hello")
     return render(request, 'home.html', context={'todo_entries': todo})
+
+@login_required
+def profile(request):
+    args = {'user': request.user}
+    return render(request, 'home.html', context={'user': args})
