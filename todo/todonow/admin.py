c99b3fb27b006cdaccabea7ec7fd87051fdf4870
Gareth Perkins <g.perkins@rcvs.org.uk>
2019-10-13 15:22:14 +0100

    chore: added user profiles

--- todo/todonow/admin.py
+++ todo/todonow/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import Todo
+from .models import Todo, UserProfile
 
 
-class TodoAdmin(admin.ModelAdmin):  # add this
+class TodoAdmin(admin.ModelAdmin):
       list_display = ('title', 'description', 'completed')
 
+class User_Profile(admin.ModelAdmin):
+      list_display = ('user', 'image')
+
 admin.site.register(Todo, TodoAdmin)
+admin.site.register(UserProfile, User_Profile)
--- todo/todonow/models.py
+++ todo/todonow/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Todo(models.Model):
@@ -11,3 +11,15 @@ class Todo(models.Model):
 
     def _str_(self):
         return self.title
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile_image', blank=True)
+
+    def _str_(self):
+        return self.user.first_name
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+post_save.connect(create_profile, sender=User)
