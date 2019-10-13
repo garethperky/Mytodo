dd3ced7c43c5d35e40a730a7b64fa1f1e0fdea41
Gareth Perkins <g.perkins@rcvs.org.uk>
2019-10-13 12:40:27 +0100

    updated urls

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('home/', include('todonow.urls')),
+    path('', include('todonow.urls')),
     path('admin/', admin.site.urls),
 ]
