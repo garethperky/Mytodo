bf63682cf5e19f8efb1162bcb62fe62ead2b87b7
Gareth Perkins <g.perkins@rcvs.org.uk>
2019-10-27 22:32:10 +0000

    feat: limited values to list of 1-10

--- todo/todonow/models.py
+++ todo/todonow/models.py
@@ -2,13 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
+VALUE_CHOICES = (
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5),
+    (6, 6),
+    (7, 7),
+    (8, 8),
+    (9, 9),
+    (10, 10),
+
+)
 
 class Todo(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    value = models.IntegerField(null=False)
+    user = models.ForeignKey(User, limit_choices_to={'groups__name': "Kids"}, on_delete=models.CASCADE)
+    value = models.IntegerField(null=False, choices=VALUE_CHOICES)
 
     def __str__(self):
         return self.title
